75f4f15d2e4b750aef5bad2e5ea3df9eff4d684e
ToF- <cthibaut@octo.com>
2015-04-18 11:21:54 +0200

    iteration #2 20 minutes

--- FruitShop.hs
+++ FruitShop.hs
@@ -7,11 +7,15 @@ process :: [String] -> [Int]
 process ss = total  ss 
 
 total :: [String] -> [Int]
-total = tail . scanl addProduct 0
+total =  map fst . tail . scanl addProduct (0,[]) 
     where
-    addProduct :: Int -> String -> Int 
-    addProduct t = (+t) . price
-    price s = case lookup s fruits of
+    addProduct :: (Int,[String]) -> String -> (Int,[String]) 
+    addProduct (t,ps) s = (t + findPrice s - reduction s ps, s:ps) 
+    findPrice s = case lookup s fruits of
         Just p -> p
         Nothing -> error $ s ++ " ???"
-    fruits = [("Pommes",100),("Cerises",75)]
+    reduction :: String -> [String] -> Int
+    reduction "Cerises" ps  = ((*20) . (`mod`2) . length . filter ((==)"Cerises")) ps
+    reduction _ _ = 0 
+
+    fruits = [("Pommes",100),("Bananes",150),("Cerises",75)]
--- Specs.hs
+++ Specs.hs
@@ -1,9 +1,18 @@
 import Test.Hspec
 import FruitShop
 
+p = "Pommes"
+c = "Cerises"
+b = "Bananes"
+
 main = hspec $ do
     describe "Fruit Shop process" $ do
         it "should add product prices and sum" $ do
-            process ["Pommes"] `shouldBe` [100]
-            process ["Pommes","Cerises"] `shouldBe` [100,175]
-            process ["Pommes","Cerises","Cerises"] `shouldBe` [100,175,250]
+            process [p] `shouldBe` [100]
+            process [b] `shouldBe` [150]
+            process [p,c] `shouldBe` [100,175]
+
+        it "should allow for reductions" $ do
+            process [p,c,c] `shouldBe` [100,175,230]
+            process [c,c,c,c] `shouldBe` [75,130,205,260]
+            process [c,p,c,b,c,c,p] `shouldBe` [75,175,230,380,455,510,610]
